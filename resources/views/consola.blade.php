1f74d306f71993409dde15debc0baa57fe9455a6
arialejandro <o.1@live.com.mx>
2020-09-17 20:27:15 -0500

    Consola

---
+++ resources/views/consola.blade.php
@@ -0,0 +1,14 @@
+<div class="col-md-4">
+<div class="card text-center bg-dark text-white">
+  <div class="card-header bg-dark text-white d-flex justify-content-between align-items-center">
+    Soy un Usuario
+    <button class="btn btn-danger" ><i class="far fa-trash-alt"></i></button>
+  </div>
+  <img src="" class="card-img-top">
+  <div class="card-body">
+    <p>hombre</p>
+    <p>29</p>
+    <a href='#' class="btn btn-info btn-block">Editar Usuario</a>
+  </div>
+</div>
+</div>
